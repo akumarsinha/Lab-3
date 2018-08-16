64b59e6ee9e3b4f0db24361dbef8a662ba85e191
akumarsinha <42178849+akumarsinha@users.noreply.github.com>
2018-08-16 14:43:08 +0000

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+            //declaring the variables
+            float a,b;
+            //initializing the variables
+            a=25;
+            b=48;
+            cout<<"the first and second variables are "<<a>>" and "<<b<<endl;
+            //displaying the functions
+            cout<<"the sum is "<<a+b<<endl;
+            cout<<"the difference is "<<b-a<<endl;
+            cout<<"the division is "<<b/a<<endl;
+            cout<<"the product is "<a*b<<endl;
+            int c=35,d=20;
+            cout<<"the modulus is "<<c%d<<endl;|
+            return 0;
+}
+
