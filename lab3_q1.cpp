926d9f1b9c8416f3b391c61713622c1556bb2deb
akumarsinha <42178849+akumarsinha@users.noreply.github.com>
2018-08-16 14:28:06 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,33 @@
+//include the necessary librabies
+#include <iostream>
+using namespace std;
+//declaring the main functions
+int main()
+{
+           //declare variables of all types
+           int integer;
+           float floating;|
+           double doub;
+           char character;
+           bool boolean:
+           //initializing them
+           integer=1;
+           floating=0.5;
+           doub=0.123456
+           character='D';
+           boolean=true;
+           //outputing all the values
+           cout<<integer<<endl;
+           cout<<floating<<endl;
+           cout<<doub<<endl;
+           cout<<character<<endl;
+           cout<<boolean<<endl;
+           //displaying the size of all variables
+           cout<<"size of integer = "<<sizeof(integer)<<endl;
+           cout<<"size of floating = "<<sizeof(floating)<<endl;
+           cout<<"size of doub = "<<sizeof(doub)<<endl;
+           cout<<"size of character = "<<sizeof(character);<<endl;
+           cout<<"size of boolean = "<<sizeof(boolean);<<endl;
+           //returning 0
+           return 0;
+}
